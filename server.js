ecb0702bfa47b01f780d796b14b9d0094d35c18e
Jacob Collard <jacob@thorsonlinguistics.com>
2021-03-30 21:44:19 -0400

    fix(deploy): trying a different server option

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+
+const app = express();
+
+app.use(compression());
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
