8d9820f32ff8fd640adee9834f92a04f4e6f6fe3
Jacob Collard <jacob@thorsonlinguistics.com>
2021-03-30 22:42:21 -0400

    fix(deploy): fixed server entry

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(compression());
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mtd/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
