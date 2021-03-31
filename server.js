65f10c4f58a6b8273ed8b03d4182cf621463f555
Jacob Collard <jacob@thorsonlinguistics.com>
2021-03-31 16:31:11 -0400

    fix(deploy): fixed deployment errors

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 
+const PORT = process.env.PORT || 4000;
+
 const app = express();
 
 app.use(compression());
-app.use(express.static(__dirname + 'dist/mtd/'));
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mtd/index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
+app.use('/', express.static(path.resolve(__dirname, 'dist/mtd')));
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
