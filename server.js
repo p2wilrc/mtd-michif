ca170dcbc5f6cfd405ed8c04a3c0b745a62c8df4
Jacob Collard <jacob@thorsonlinguistics.com>
2021-03-30 23:00:09 -0400

    fix(deploy): one more time

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(compression());
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/mtd/index.html'));
 });
 
