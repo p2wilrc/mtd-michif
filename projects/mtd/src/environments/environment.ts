2e691f75029dabac585e261b7793b22fa7d2eee8
David Huggins-Daines <dhd@ecolingui.ca>
2023-04-06 18:17:51 -0400

    fix: always add a leading . to i18n files to make base-href work

--- projects/mtd/src/environments/environment.ts
+++ projects/mtd/src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   envName: 'DEV',
   production: false,
   test: false,
-  i18nPrefix: '',
+  i18nPrefix: '.',
   apiBaseURL: 'http://localhost:5000',
   remoteData: false,
   remoteConfig: false,
