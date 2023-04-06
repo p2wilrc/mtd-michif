79c5d54a53d2b28d5c61e0d940b7a8ec670a285b
David Huggins-Daines <dhd@ecolingui.ca>
2023-04-06 18:55:17 -0400

    chore: update angular/model
    chore: update typescript
    
    chore: more updates
    
    chore: add browserslist
    
    chore: update more angulars
    
    chore: update all the things
    
    chore: npm audit fix
    
    chore: update @angular/core
    
    chore: update @angular/cli
    
    chore: update @angular/cdk and @angular/material
    
    chore: update @ngrx/store
    
    chore: update some other dependencies
    
    chore: npm audit fix

--- projects/mtd/src/app/pages/construction/construction/construction.component.spec.ts
+++ projects/mtd/src/app/pages/construction/construction/construction.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/httmp/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { ConstructionComponent } from './construction.component';
@@ -7,12 +7,14 @@ describe('ConstructionComponent', () => {
   let component: ConstructionComponent;
   let fixture: ComponentFixture<ConstructionComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, TranslateModule.forRoot()],
-      declarations: [HomeComponent]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule, TranslateModule.forRoot()],
+        declarations: [HomeComponent]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
--- projects/mtd/src/app/shared/entry-list/entry-list.component.spec.ts
+++ projects/mtd/src/app/shared/entry-list/entry-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { EntryListComponent } from './entry-list.component';
@@ -7,16 +7,18 @@ describe('EntryListComponent', () => {
   let component: EntryListComponent;
   let fixture: ComponentFixture<EntryListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-        TranslateModule.forRoot(),
-        SharedModule
-      ],
-      declarations: [EntryListComponent]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          HttpClientTestingModule,
+          TranslateModule.forRoot(),
+          SharedModule
+        ],
+        declarations: [EntryListComponent]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EntryListComponent);
