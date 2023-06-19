f4b1bc04adc3d400245656d5b5aca80ec1f279ca
David Huggins-Daines <dhd@ecolingui.ca>
2023-06-19 19:31:17 -0400

    fix: satisfy angular and its lust for power for which lives are squandered

--- projects/mtd/src/app/shared/pronunciation-guide/pronunciation-guide.component.ts
+++ projects/mtd/src/app/shared/pronunciation-guide/pronunciation-guide.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'mtd-pronunciation-guide',
@@ -6,8 +6,4 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./pronunciation-guide.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PronunciationGuideComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class PronunciationGuideComponent {}
