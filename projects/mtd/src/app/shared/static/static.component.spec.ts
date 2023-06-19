6db4d996afc101b6370d38cb39120b6e02c15c8f
David Huggins-Daines <dhd@ecolingui.ca>
2023-06-19 19:20:58 -0400

    refactor: use a common template for static-like pages

---
+++ projects/mtd/src/app/shared/static/static.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { SharedModule } from '../../../shared/shared.module';
+
+import { StaticComponent } from './static.component';
+
+describe('StaticComponent', () => {
+  let component: StaticComponent;
+  let fixture: ComponentFixture<StaticComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          SharedModule,
+          NoopAnimationsModule,
+          TranslateModule.forRoot()
+        ],
+        declarations: [StaticComponent]
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StaticComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
