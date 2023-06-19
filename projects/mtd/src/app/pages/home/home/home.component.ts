a50b395832a7c19e90e167615619f0cde1a7b413
David Huggins-Daines <dhd@ecolingui.ca>
2023-06-19 19:22:24 -0400

    refactor: use common mtd-static component

--- projects/mtd/src/app/pages/home/home/home.component.ts
+++ projects/mtd/src/app/pages/home/home/home.component.ts
@@ -1,16 +1,15 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import {
-  MtdService,
-  ROUTE_ANIMATIONS_ELEMENTS
-} from '../../../core/core.module';
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
 
 @Component({
   selector: 'mtd-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: [
+    '../../../shared/static/static.component.scss',
+    './home.component.scss'
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
-  constructor(public mtdService: MtdService) {}
 }
