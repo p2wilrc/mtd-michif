1388b0e3ab07a92b812b1e9c661041c1696c2e3b
David Huggins-Daines <dhd@ecolingui.ca>
2023-07-12 14:38:48 -0400

    test: stub files to make tests happy

---
+++ projects/mtd/src/assets/js/config-michif.js
@@ -0,0 +1,55 @@
+var config = {
+  L1: {
+    name: 'Michif',
+    lettersInLanguage: [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z'
+    ],
+    transducers: {
+      'michif-approx': [
+        { ae: 'a' },
+        { aw: 'a' },
+        { uh: 'a' },
+        { ay: 'e' },
+        { ee: 'e' },
+        { oo: 'u' },
+        { ou: 'u' },
+        { au: 'u' },
+        { uw: 'u' },
+        { hk: 'k' },
+        { ht: 't' },
+        { hp: 'p' },
+        { '\\-': '' },
+        { '\u2019': '' },
+        { "'": '' }
+      ]
+    }
+  },
+  L2: { name: 'English' },
+  build: '202307121433',
+  audio_path: 'assets/'
+};
