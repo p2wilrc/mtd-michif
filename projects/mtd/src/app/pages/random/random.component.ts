1392ec6482f9b764c43633db852dd3af146c0a69
David Huggins-Daines <dhd@ecolingui.ca>
2023-08-24 11:46:22 -0400

    feat: adjust number of random entries to screen size
    the implementation is sub-optimal but it will do for now.

--- projects/mtd/src/app/pages/random/random.component.ts
+++ projects/mtd/src/app/pages/random/random.component.ts
@@ -1,7 +1,8 @@
 import {
   Component,
   ChangeDetectorRef,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  HostListener
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
@@ -25,6 +26,7 @@ export class RandomComponent {
   unsubscribe$ = new Subject<void>();
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   show?: string;
+  nRandom = this.guessNumEntries();
   constructor(
     private mtdService: MtdService,
     private route: ActivatedRoute,
@@ -38,11 +40,51 @@ export class RandomComponent {
       });
   }
 
+  @HostListener('window:resize')
+  handleResize() {
+    const newCount = this.guessNumEntries();
+    if (newCount != this.nRandom) {
+      // No real reason to use an Observable here, right?
+      this.nRandom = newCount;
+      this.ref.markForCheck();
+    }
+  }
+
+  /**
+   * Guess a reasonable number of entries based on the viewport size.
+   *
+   * This can't be exact because the entries may expand for long definitions.
+   */
+  guessNumEntries(): number {
+    /* FIXME: for now, we use the viewport size and not the size of
+       the content__text element - this is not very readable and has
+       to match the defitions in `styles-variables.scss` and
+       `single.component.scss`! */
+    const vw = document.documentElement.clientWidth;
+    const vh = document.documentElement.clientHeight;
+    const is_phone = vw < 600; // NOTE: not specific to phones!
+    const toolbar_height = is_phone ? 56 : 64; // Thanks, Google!
+    const margin = is_phone ? 16 : 32;
+    const header_height = is_phone ? 32 : 48;
+    const padding = is_phone ? 10 : 15;
+    const guide_height = 18; // NOTE: just a guess!
+    const height =
+      vh -
+      toolbar_height -
+      2 * margin -
+      header_height -
+      2 * padding -
+      guide_height;
+
+    // FIXME: Really unsure where those extra 6 pixels come from.  Thanks, Google.
+    return Math.floor(is_phone ? height / 96 : height / 56);
+  }
+
   ngOnDestroy() {
     this.unsubscribe$.next();
   }
 
   getRandom() {
-    this.entries$ = this.mtdService.getRandom$(10);
+    this.entries$ = this.mtdService.getRandom$(this.nRandom);
   }
 }
